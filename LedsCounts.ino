645d245db82920c7b9fe0403a058c63cd337d873
Tarunika <taruni.eragon@gmail.com>
2019-09-27 22:29:05 +0200

    Everything works now :O

--- LedsCounts.ino
+++ LedsCounts.ino
@@ -10,86 +10,58 @@
 // https://github.com/FastLED/FastLED/wiki/Overview
 #define LED_TYPE    WS2812B
 #define BRIGHTNESS          50
-int fsrAnalogPin = 0; // FSR is connected to analog 0
-int LEDpin = 11;      // connect Red LED to pin 11 (PWM pin)
-int fsrReading;      // the analog reading from the FSR resistor divider
 int LEDbrightness;
-
-
+int sensor = 2;              // the pin that the sensor is atteched to
+int state[NUM_LEDS] = {LOW};             // by default, no motion detected
+int val[NUM_LEDS] = {0};                 // variable to store the sensor status (value)
+int LedFlag = 0;
 
 // this creates an LED array to hold the values for each led in your strip
 CRGB leds[NUM_LEDS];
 
 void setup()
 {
-  
+
   // the wiki features a much more basic setup line:
+
+  // initalize LED as an output
+
+  pinMode(sensor, INPUT);    // initialize sensor as an input
   FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS);
   FastLED.setBrightness(BRIGHTNESS);
-  pinMode(LEDpin, OUTPUT);
+  pinMode(DATA_PIN, OUTPUT);
   Serial.begin(9600);
 
 
 }
-void loop() 
+void loop()
 {
-    fsrReading = analogRead(fsrAnalogPin);
-  Serial.print("Analog reading = ");
-  Serial.println(fsrReading);
- 
-  // we'll need to change the range from the analog reading (0-1023) down to the range
-  // used by analogWrite (0-255) with map!
-  //LEDbrightness = map(fsrReading, 0, 1023, 0, 255);
-  // LED gets brighter the harder you press
-  //analogWrite(LEDpin, LEDbrightness);
-  leds[0] = CRGB :: Green;
- 
-  //delay(10);
 
-  if(fsrReading > 0){
-    for( int i = 1; i<7; i++){
-      
-      leds[i] = CRGB :: Green;
+  for (int i = 0; i < NUM_LEDS; ) {
+    val[i] = digitalRead(sensor);   // read sensor value
+    if (val[i] == HIGH && state[i] == LOW) {
+      Serial.println("Motion detected!");
+      state[i] = HIGH;       // update variable state to HIGH
     }
-   // EVERY_N_SECONDS( 10 ) { nextPattern(); }
-  
-
-  }
-  else {
-    for( int i = 0; i<7; i++){
-      
-      leds[i] = 0;
+    if (val[i] == HIGH && state[i] == HIGH) {           // check if the sensor is HIGH
+      leds[i] = CRGB:: Green;   // turn LED ON
     }
     
+    FastLED.show();
+    delay(2000);
+
+   if ( digitalRead(sensor) == HIGH){
+    i = i+1;
+    Serial.print(i);
+   }
+  
+
   }
-  
-  
-  
-//  leds[0] = CRGB::Red;
-//  leds[1] = CRGB::Red;
-//
-//  leds[2] = CRGB::Green;
-//  leds[3] = CRGB::Blue;
 
-  FastLED.show();
-
- // delay(50);
-}
-void nextPattern()
-{
-  // add one to the current pattern number, and wrap around at the end
-  int i=0;
-  for(i=0;i<7;i++)
+  for( int i=0;i<NUM_LEDS;i++)
   {
-    if(leds[i].r){
-      leds[i] = CRGB::Green;
-    }
-    else if(leds[i].g){
-      leds[i] = CRGB::Blue;
-    }
-    else{
-      leds[i] = CRGB::Red;
+    leds[i]= 0;
   }
-  }
-  
+
+
 }
