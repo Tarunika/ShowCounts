e4b765d0bed4a5c902a879e9bc7e074882ce556c
roosaurora <49948358+roosaurora@users.noreply.github.com>
2019-10-14 11:42:23 +0200

    Add files via upload

--- LedsCounts.ino
+++ LedsCounts.ino
@@ -1,5 +1,4 @@
 #include <FastLED.h>
-
 // fast led constants
 #define DATA_PIN    3
 #define RED_PIN   6   // red output strip
@@ -8,21 +7,21 @@
 #define COLOR_ORDER GRB      // if colors are mismatched; change this
 #define MAX_POINTS    16       // change to the number of LEDs in your strip
 #define amountOfLeds 37 //refers to the amount of leds circling around each hole
+#define LED_TYPE    WS2812B
+#define BRIGHTNESS          50
 
 
 // change WS2812B to match your type of LED, if different
 // list of supported types is here:
 // https://github.com/FastLED/FastLED/wiki/Overview
-#define LED_TYPE    WS2812B
-#define BRIGHTNESS          50
 int LEDbrightness;
-int redSensor = 2;                        // the pin that the redSensor is atteched to
-int greenSensor = 4;
-int blueSensor = 5;
+int midSensor = 2;                        // the pin that the midSensor is atteched to
+int leftSensor = 4;
+int rightSensor = 5;
 int state[MAX_POINTS] = {LOW};              // by default, no motion detected
-int redState[MAX_POINTS] = {0};             // variable to store the redSensor status (value)
-int greenState[MAX_POINTS] = {0};           // variable to Store the greenSensor status (value)
-int blueState[MAX_POINTS] = {0};            // variable to store the blueSensor status (value)
+int midState[MAX_POINTS] = {0};             // variable to store the midSensor status (value)
+int leftState[MAX_POINTS] = {0};           // variable to Store the leftSensor status (value)
+int rightState[MAX_POINTS] = {0};            // variable to store the rightSensor status (value)
 int LedFlag = 0;
 int index = 0;
 int circlingIndex[3] = {0, 1, 2};
@@ -30,9 +29,10 @@ int prevIndex[3] = {0, 0, 0};
 
 // this creates an LED array to hold the values for each led in your strip
 CRGB pointLeds[MAX_POINTS];
-CRGB redLeds[amountOfLeds];
-CRGB greenLeds[amountOfLeds];
-CRGB blueLeds[amountOfLeds];
+CRGB midLeds[amountOfLeds];
+CRGB leftLeds[amountOfLeds];
+CRGB rightLeds[amountOfLeds];
+CRGB colors[3] = {CRGB :: Red, CRGB :: Green, CRGB :: Blue};
 
 void setup()
 {
@@ -40,19 +40,20 @@ void setup()
   // the wiki features a much more basic setup line:
 
   // initalize LED as an output
-  pinMode(redSensor, INPUT);    // initialize redSensor as an input
-  pinMode(greenSensor, INPUT);
-  pinMode(blueSensor, INPUT);
+  pinMode(midSensor, INPUT);    // initialize midSensor as an input
+  pinMode(leftSensor, INPUT);
+  pinMode(rightSensor, INPUT);
   FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(pointLeds, MAX_POINTS);
-  FastLED.addLeds<LED_TYPE, RED_PIN, COLOR_ORDER>(redLeds, amountOfLeds);
-  FastLED.addLeds<LED_TYPE, BLUE_PIN, COLOR_ORDER>(blueLeds, amountOfLeds);
-  FastLED.addLeds<LED_TYPE, GREEN_PIN, COLOR_ORDER>(greenLeds, amountOfLeds);
+  FastLED.addLeds<LED_TYPE, RED_PIN, COLOR_ORDER>(midLeds, amountOfLeds);
+  FastLED.addLeds<LED_TYPE, BLUE_PIN, COLOR_ORDER>(rightLeds, amountOfLeds);
+  FastLED.addLeds<LED_TYPE, GREEN_PIN, COLOR_ORDER>(leftLeds, amountOfLeds);
   FastLED.setBrightness(BRIGHTNESS);
   pinMode(DATA_PIN, OUTPUT);
   pinMode(RED_PIN, OUTPUT);
   pinMode(GREEN_PIN, OUTPUT);
   pinMode(BLUE_PIN, OUTPUT);
   Serial.begin(9600);
+  randomSeed(analogRead(0));
 
 }
 
@@ -64,27 +65,38 @@ void addPoint(int colorState[], CRGB color) {
   index = index + 1;
 }
 
+void shuffleColors() {
+  // Random number generator
+
+  for (int i = 0; i < 3; i++) {
+    int randomPosition = random(3);
+    CRGB temp = colors[i]; //test, should be CRGB
+    colors[i] = colors[randomPosition];
+    colors[randomPosition] = temp;
+  }
+}
+
 void fireInDaHole(CRGB color) {
-  Serial.println(color);
   for (int i = 0; i < amountOfLeds; i++) {
-    redLeds[i] = color;
-    greenLeds[i] = color;
-    blueLeds[i] = color;
+    midLeds[i] = color;
+    leftLeds[i] = color;
+    rightLeds[i] = color;
   }
   FastLED.show();
+  shuffleColors();
   delay(800);
 }
 
 void updateLights() {
   for (int i = amountOfLeds - 1; i > amountOfLeds - 3; i--) {
-    redLeds[i] = 0;
-    greenLeds[i] = 0;
-    blueLeds[i] = 0;
+    midLeds[i] = 0;
+    leftLeds[i] = 0;
+    rightLeds[i] = 0;
   }
   for (int i = 0; i < 3; i++) {
-    redLeds[prevIndex[i]] = 0;
-    blueLeds[prevIndex[i]] = 0;
-    greenLeds[prevIndex[i]] = 0;
+    midLeds[prevIndex[i]] = 0;
+    rightLeds[prevIndex[i]] = 0;
+    leftLeds[prevIndex[i]] = 0;
   }
 
   prevIndex[0] = circlingIndex[0];
@@ -97,9 +109,9 @@ void updateLights() {
     circlingIndex[2] = 2;
   } else {
     for (int i = 0; i < 3; i++) {
-      redLeds[circlingIndex[i]] = CRGB :: Red;
-      blueLeds[circlingIndex[i]] = CRGB :: Blue;
-      greenLeds[circlingIndex[i]] = CRGB :: Green;
+      midLeds[circlingIndex[i]] = colors[0];
+      leftLeds[circlingIndex[i]] = colors[1];
+      rightLeds[circlingIndex[i]] = colors[2];
       circlingIndex[i] = circlingIndex[i] + 3;
     }
   }
@@ -110,27 +122,30 @@ void updateLights() {
 void loop()
 {
   //updateLights();
-  redState[index] = digitalRead(redSensor);   // read redSensor value
-  blueState[index] = digitalRead(blueSensor); // read blueSensor value
-  greenState[index] = digitalRead(greenSensor); // read greenSensor value
-  if ((redState[index] == HIGH || greenState[index] == HIGH || blueState[index] == HIGH) && state[index] == LOW) {
+  midState[index] = digitalRead(midSensor);   // read midSensor value
+  rightState[index] = digitalRead(rightSensor); // read rightSensor value
+  leftState[index] = digitalRead(leftSensor); // read leftSensor value
+  if ((midState[index] == HIGH || leftState[index] == HIGH || rightState[index] == HIGH) && state[index] == LOW) {
     state[index] = HIGH;       // update the current individual led state to HIGH
   } else {
     updateLights();
   }
 
   if (state[index] == HIGH) {
-    if (redState[index] == HIGH) {
-      fireInDaHole(CRGB :: Red);
-      addPoint(redState, CRGB :: Red);
+    if (midState[index] == HIGH) {
+      CRGB prevColor = colors[0];
+      fireInDaHole(prevColor);
+      addPoint(midState, prevColor);
     }
-    if (greenState[index] == HIGH) {
-      fireInDaHole(CRGB :: Green);
-      addPoint(greenState, CRGB :: Green);
+    if (leftState[index] == HIGH) {
+      CRGB prevColor = colors[1];
+      fireInDaHole(prevColor);
+      addPoint(leftState, prevColor);
     }
-    if (blueState[index] == HIGH) {
-      fireInDaHole(CRGB :: Blue);
-      addPoint(blueState, CRGB :: Blue);
+    if (rightState[index] == HIGH) {
+      CRGB prevColor = colors[2];
+      fireInDaHole(prevColor);
+      addPoint(rightState, prevColor);
     }
   }
 
